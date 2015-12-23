0c74016cf209322c48173bcfd38e7f5ba5dbffaf
MAGIER Dan <magier.dan.pro@gmail.com>
2015-12-23 12:37:37 +0100

    GildedRose : modifications for Age Bride only

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -9,6 +9,25 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+
+            if (items[i].name.equals("Aged Brie")) {
+
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
+
+                items[i].sellIn = items[i].sellIn - 1;
+
+                if (items[i].sellIn < 0) {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                }
+
+                continue;
+            }
+
+
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
